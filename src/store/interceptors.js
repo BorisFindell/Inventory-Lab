b165c0ab1564f7601cd14b24086493141b4d3b22
mdinoto <marilendlf@gmail.com>
2022-06-10 14:44:52 +0200

    request interceptors

---
+++ src/store/interceptors.js
@@ -0,0 +1,19 @@
+import * as fetchIntercept from 'fetch-intercept';
+import store from '.'
+
+fetchIntercept.register({
+    request: function (url, config) {
+        config.headers = {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + store.state.userObj?.jwt
+        }
+        return [url, config];
+    },
+
+    response: function (response) {
+        if (!response.ok && response.status == 401) {
+            store.commit('logOut')
+        }
+        return response;
+    }
+});
